f84ec4bc6de202eb6a4c545f2a90b7a0ee469054
Rafik Kafel <kafelr@hotmail.com>
2012-10-05 00:05:27 -0400

    Initial commit of bushFrog application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BushFrog::Application.config.secret_token = '012f998918425f1951c8ff6737fd10bece3f9b6e157d92929189ac53a953b6584f1804cf692a78529efc976d2952386be8aa36aa7d07c588caba5748d8b49dd6'
